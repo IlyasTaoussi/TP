c34f944a45c6a808835576ef594c607dea18c475
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-02-14 17:12:42 +0100

    Update Module.java

--- TP/src/main/java/com/tp/TP/ressource/Module.java
+++ TP/src/main/java/com/tp/TP/ressource/Module.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Module implements Serializable{
@@ -19,7 +19,7 @@ public class Module implements Serializable{
 	
 	private String nomModule;
 	
-	@OneToMany
+	@ManyToMany
 	private ArrayList<Specialite> specs;
 
 	public Module(String nomModule) {
