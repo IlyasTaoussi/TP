7a2b7a6d84837ccd039bfcee6bec247083dfa4f2
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-02-14 20:59:44 +0100

    Update TpApplication.java

--- TP/src/main/java/com/tp/TP/process/TpApplication.java
+++ TP/src/main/java/com/tp/TP/process/TpApplication.java
@@ -1,7 +1,9 @@
 package com.tp.TP.process;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TpApplication {
@@ -9,5 +11,9 @@ public class TpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TpApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner init() {
+		return (args)->{};
+	}
 }
