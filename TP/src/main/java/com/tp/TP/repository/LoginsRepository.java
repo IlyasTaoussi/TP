207270a9a1faee00e593b4ce16ecaf32090d2277
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-02-16 20:37:04 +0100

    Update LoginsRepository.java

--- TP/src/main/java/com/tp/TP/repository/LoginsRepository.java
+++ TP/src/main/java/com/tp/TP/repository/LoginsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.tp.TP.ressource.Logins;
 
 public interface LoginsRepository extends CrudRepository<Logins, Integer>{
-
+	Logins findByMailAndPassword(String mail, String password);
 }
