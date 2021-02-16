aae5d8434b149c28601c24ab38c3944fb1494178
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-02-16 20:05:43 +0100

    Update ModuleRepository.java

--- TP/src/main/java/com/tp/TP/repository/ModuleRepository.java
+++ TP/src/main/java/com/tp/TP/repository/ModuleRepository.java
@@ -1,6 +1,7 @@
 package com.tp.TP.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import com.tp.TP.ressource.Module;
 
 public interface ModuleRepository extends CrudRepository<Module, Integer> {
 	
