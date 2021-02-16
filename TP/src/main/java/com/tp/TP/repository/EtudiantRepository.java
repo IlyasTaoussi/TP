c9aa7b7599a675efeda267c7b5373ed2f9e3e58a
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-02-16 20:37:02 +0100

    Update EtudiantRepository.java

--- TP/src/main/java/com/tp/TP/repository/EtudiantRepository.java
+++ TP/src/main/java/com/tp/TP/repository/EtudiantRepository.java
@@ -1,9 +1,17 @@
 package com.tp.TP.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.tp.TP.ressource.Etudiant;
+import com.tp.TP.ressource.Logins;
+import com.tp.TP.ressource.Specialite;
 
 public interface EtudiantRepository extends CrudRepository<Etudiant, Integer> {
+	List<Etudiant> findByNomEtu(String nomEtu);
+	List<Etudiant> findByPrenomEtu(String prenomEtu);
+	Etudiant findByLoginEtu(Logins loginEtu);
+	List<Etudiant> findBySpec(Specialite spec);
 	
 }
