b45780f1527a58ee8c898c76b061d7e6526555a4
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-02-16 20:37:11 +0100

    Update ProfesseurRepository.java

--- TP/src/main/java/com/tp/TP/repository/ProfesseurRepository.java
+++ TP/src/main/java/com/tp/TP/repository/ProfesseurRepository.java
@@ -1,9 +1,15 @@
 package com.tp.TP.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.tp.TP.ressource.Logins;
 import com.tp.TP.ressource.Professeur;
 
 public interface ProfesseurRepository extends CrudRepository<Professeur, Integer>{
-
+	List<Professeur> findByNomProf(String nomProf);
+	List<Professeur> findByPrenomProf(String prenomProf);
+	Professeur findByModule(Module module);
+	Professeur findByLoginProf(Logins loginProf);
 }
