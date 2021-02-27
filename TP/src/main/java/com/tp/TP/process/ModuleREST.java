48ba14498bccab0d7abc84f116a90a31528fbcf6
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-02-27 10:50:44 +0100

    Update ModuleREST.java

--- TP/src/main/java/com/tp/TP/process/ModuleREST.java
+++ TP/src/main/java/com/tp/TP/process/ModuleREST.java
@@ -4,7 +4,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +20,7 @@ public class ModuleREST {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Module addModule(@QueryParam("nom") String nom){
-        Module m = new Module(nom);
+    public Module addModule(Module m){
         moduleRepository.save(m);
         return m;
     }
