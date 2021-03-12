8a4478e63864f883cde43ad0f6d3d9e7c831cb59
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-12 10:54:32 +0100

    Update SpecialiteRepository.java

--- TP/src/main/java/com/tp/TP/repository/SpecialiteRepository.java
+++ TP/src/main/java/com/tp/TP/repository/SpecialiteRepository.java
@@ -1,13 +1,12 @@
 package com.tp.TP.repository;
 
-import java.util.List;
+
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.tp.TP.ressource.Specialite;
 
 public interface SpecialiteRepository extends CrudRepository<Specialite, Integer> {
-	List<Specialite> findByModule(Module module);
 	Specialite findByNomSpec(String nomSpec);
 	
 }
