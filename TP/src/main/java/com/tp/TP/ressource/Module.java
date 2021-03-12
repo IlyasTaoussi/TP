a4044969969d8a5c39fa5ca6ca544193fd98e9c4
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-12 10:54:38 +0100

    Update Module.java

--- TP/src/main/java/com/tp/TP/ressource/Module.java
+++ TP/src/main/java/com/tp/TP/ressource/Module.java
@@ -1,13 +1,14 @@
 package com.tp.TP.ressource;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Module implements Serializable{
@@ -19,12 +20,21 @@ public class Module implements Serializable{
 	
 	private String nomModule;
 	
-	@ManyToMany
-	private ArrayList<Specialite> specs;
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Specialite spe;
 
-	public Module(String nomModule) {
+	public Module(String nomModule, Specialite spe) {
 		super();
 		this.nomModule = nomModule;
+		this.spe = spe;
+	}
+
+	public Specialite getSpe() {
+		return spe;
+	}
+
+	public void setSpe(Specialite spe) {
+		this.spe = spe;
 	}
 
 	public Module() {
