a1b73f0342127ef41b6c42ecd97068245bb81f3d
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-12 12:21:05 +0100

    Update SpecialiteREST.java

--- TP/src/main/java/com/tp/TP/process/SpecialiteREST.java
+++ TP/src/main/java/com/tp/TP/process/SpecialiteREST.java
@@ -1,14 +1,21 @@
 package com.tp.TP.process;
 
+import java.util.Optional;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 
+import com.tp.TP.repository.ModuleRepository;
 import com.tp.TP.repository.SpecialiteRepository;
+import com.tp.TP.ressource.Module;
 import com.tp.TP.ressource.Specialite;
 
 @Path("specialites")
@@ -16,11 +23,13 @@ public class SpecialiteREST {
 	
 	@Autowired
 	private SpecialiteRepository specialiteRepository;
+	@Autowired
+	private ModuleRepository moduleRepository;
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Specialite addSpecialite(Specialite sp){
+    public Specialite addSpecialit(Specialite sp){
         specialiteRepository.save(sp);
         return sp;
     }
