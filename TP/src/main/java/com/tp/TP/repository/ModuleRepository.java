aadb0173463f9d8cfac32d7334f7a4e4abc8a386
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-14 20:53:00 +0100

    Update ModuleRepository.java

--- TP/src/main/java/com/tp/TP/repository/ModuleRepository.java
+++ TP/src/main/java/com/tp/TP/repository/ModuleRepository.java
@@ -3,10 +3,13 @@ package com.tp.TP.repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import com.tp.TP.ressource.Module;
+import com.tp.TP.ressource.Specialite;
 
 public interface ModuleRepository extends CrudRepository<Module, Integer> {
 	Module findByNomModule(String nomModule);
-	
+	List<Module> findBySpe(Specialite spe);
 }
