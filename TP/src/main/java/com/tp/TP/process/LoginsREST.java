7cbb214d5683edf1e65bdc27026879fdbbbb1904
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-14 20:52:44 +0100

    Update LoginsREST.java

--- TP/src/main/java/com/tp/TP/process/LoginsREST.java
+++ TP/src/main/java/com/tp/TP/process/LoginsREST.java
@@ -1,21 +1,34 @@
 package com.tp.TP.process;
 
+import java.util.Optional;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.tp.TP.repository.EtudiantRepository;
 import com.tp.TP.repository.LoginsRepository;
+import com.tp.TP.repository.ProfesseurRepository;
+import com.tp.TP.ressource.Etudiant;
 import com.tp.TP.ressource.Logins;
+import com.tp.TP.ressource.Professeur;
 
 @Path("logins")
 public class LoginsREST {
 	
 	@Autowired
 	private LoginsRepository loginsRepository;
+	@Autowired
+	private EtudiantRepository etudiantRepository;
+	@Autowired
+	private ProfesseurRepository professeurRepository;
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -24,6 +37,30 @@ public class LoginsREST {
         loginsRepository.save(m);
         return m;
     }
+	
+	@PATCH
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	public Response setLogin(@PathParam("id") int id, Logins L) {
+		Optional<Etudiant> optE = etudiantRepository.findById(id);
+		Optional<Professeur> optP = professeurRepository.findById(id);
+		
+		if(!optE.isPresent()) {
+			if(!optP.isPresent()) {
+				return Response.status(Response.Status.NOT_FOUND).build();
+			}
+			Professeur p = optP.get();
+			p.setLogin(L);
+			professeurRepository.save(p);
+			return Response.ok(p).build();
+		}
+		
+		Etudiant e = optE.get();
+		e.setLogin(L);
+		etudiantRepository.save(e);
+		return Response.ok(e).build();
+	}
 	
 	
 }
