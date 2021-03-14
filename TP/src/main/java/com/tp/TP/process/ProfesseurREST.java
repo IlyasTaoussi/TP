a67e46a8905aa66fc526bb1e5dc89669151b0668
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-14 20:52:49 +0100

    Update ProfesseurREST.java

--- TP/src/main/java/com/tp/TP/process/ProfesseurREST.java
+++ TP/src/main/java/com/tp/TP/process/ProfesseurREST.java
@@ -1,6 +1,10 @@
 package com.tp.TP.process;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -24,4 +28,12 @@ public class ProfesseurREST {
         professeurRepository.save(p);
         return p;
     }
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Professeur> getAllEtudiants(){
+		List<Professeur> profs = new ArrayList<>();
+		professeurRepository.findAll().forEach(profs::add);
+		return profs;
+	}
 }
