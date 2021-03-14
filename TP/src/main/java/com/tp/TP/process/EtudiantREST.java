bdc22b128a7a03b899a2ae4632ab591472fcd10b
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-14 20:52:40 +0100

    Update EtudiantREST.java

--- TP/src/main/java/com/tp/TP/process/EtudiantREST.java
+++ TP/src/main/java/com/tp/TP/process/EtudiantREST.java
@@ -1,10 +1,13 @@
 package com.tp.TP.process;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +24,17 @@ public class EtudiantREST {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Etudiant addEtudiant(@QueryParam("nom") String nom, @QueryParam("prenom") String prenom){
-        Etudiant e = new Etudiant(nom, prenom);
-        etudiantRepository.save(e);
-        return e;
+    public Etudiant addEtudiant(Etudiant E){
+        return etudiantRepository.save(E);
     }
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Etudiant> getAllEtudiants(){
+		List<Etudiant> etudiants = new ArrayList<>();
+		etudiantRepository.findAll().forEach(etudiants::add);
+		return etudiants;
+	}
+	
+	
 }
