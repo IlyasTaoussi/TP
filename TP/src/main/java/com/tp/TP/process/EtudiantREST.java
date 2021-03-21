4cfd213fcc25c8e4dd58769604636bfca3aad608
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-21 15:18:53 +0100

    Update REST

--- TP/src/main/java/com/tp/TP/process/EtudiantREST.java
+++ TP/src/main/java/com/tp/TP/process/EtudiantREST.java
@@ -2,6 +2,7 @@ package com.tp.TP.process;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -9,17 +10,23 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tp.TP.repository.EtudiantRepository;
+import com.tp.TP.repository.LoginsRepository;
 import com.tp.TP.ressource.Etudiant;
+import com.tp.TP.ressource.LoginInput;
+import com.tp.TP.ressource.Logins;
 
 @Path("etudiants")
 public class EtudiantREST {
 	
 	@Autowired
 	private EtudiantRepository etudiantRepository;
+	@Autowired
+	private LoginsRepository loginsRepository;
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -36,4 +43,19 @@ public class EtudiantREST {
 		return etudiants;
 	}
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("logins")
+	public Response getLoginEtu(LoginInput LI) {
+		Optional<Logins> optL = loginsRepository.findByMailAndPassword(LI.getEmail(), LI.getPasswd());
+		if(!optL.isPresent())
+			Response.status(Response.Status.NOT_FOUND).build();
+		Optional<Etudiant> optE = etudiantRepository.findByLoginEtu(optL.get());
+		if(!optE.isPresent())
+			Response.status(Response.Status.NOT_FOUND).build();
+		
+		Etudiant E = optE.get();
+		return Response.ok(E).build();
+	}
 }
--- TP/src/main/java/com/tp/TP/process/ProfesseurREST.java
+++ TP/src/main/java/com/tp/TP/process/ProfesseurREST.java
@@ -2,6 +2,7 @@ package com.tp.TP.process;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -9,10 +10,14 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.tp.TP.repository.LoginsRepository;
 import com.tp.TP.repository.ProfesseurRepository;
+import com.tp.TP.ressource.LoginInput;
+import com.tp.TP.ressource.Logins;
 import com.tp.TP.ressource.Professeur;
 
 @Path("professeurs")
@@ -20,6 +25,8 @@ import com.tp.TP.ressource.Professeur;
 public class ProfesseurREST {
 	@Autowired
 	private ProfesseurRepository professeurRepository;
+	@Autowired
+	private LoginsRepository loginsRepository;
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -35,5 +42,21 @@ public class ProfesseurREST {
 		List<Professeur> profs = new ArrayList<>();
 		professeurRepository.findAll().forEach(profs::add);
 		return profs;
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("logins")
+	public Response getLoginEtu(LoginInput LI) {
+		Optional<Logins> optL = loginsRepository.findByMailAndPassword(LI.getEmail(), LI.getPasswd());
+		if(!optL.isPresent())
+			Response.status(Response.Status.NOT_FOUND).build();
+		Optional<Professeur> optP = professeurRepository.findByLoginProf(optL.get());
+		if(!optP.isPresent())
+			Response.status(Response.Status.NOT_FOUND).build();
+		
+		Professeur P = optP.get();
+		return Response.ok(P).build();
 	}
 }
